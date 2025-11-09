19ba44d467f007511e7cb695d0cd852491e49b44
aliadnankhan <114007060+aliadnankhan@users.noreply.github.com>
2025-11-09 16:49:05 -0500

    V1.04
    connect express library

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-// require module
-var connect = require('connect')
-// create app
-var app = connect()
+const express = require('express')
+const app = express()
+const port = 3000
 
-function homepage(req, res, next) 
-{
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Home Page\n')
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-function aboutme(req, res, next) 
-{
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('About Me\n')
-}
-
-function projectpage(req, res, next) 
-{
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Project Page\n')
-}
-
-function contactus(req, res, next) 
-{
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Contact Us\n')
-}
-
-app.use('/', homepage)
-app.use('/aboutme', aboutme)
-app.use('/projectpage', projectpage)
-app.use('/contactus', contactus)
-app.listen(3000)
-console.log('Server is running on local host')
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}')
+})
